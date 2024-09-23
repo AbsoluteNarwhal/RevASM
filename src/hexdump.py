806a23caad5e13bea2899796525c105f3fd748a2
Alexander Martin <absolutenarwhal@gmail.com>
2024-09-23 21:30:10 +0100

    argv

---
+++ src/hexdump.py
@@ -0,0 +1,2 @@
+def format(contents):
+    pass
--- src/main.py
+++ src/main.py
@@ -1,8 +1,25 @@
+import sys
 import help
+import vars
+import hexdump
 
 def main():
-    pass
+    args = sys.argv
+    if len(args) > 4:
+        print("Error: Too many arguments")
+        return
+    if args[1] == "--help" or args[1] == "-h":
+        if len(args) == 2:
+            help.select("")
+        help.select(args[2])
+        return
+    if args[1] == "--version" or args[1] == "-v":
+        print(f"RevASM version {vars.VERSION} - 2024 Absolute Narwhal\n")
+        return
+    if len(args) < 3:
+        print("Error: No file provided")
 
-
+    if args[1] == "hexdump":
+        hexdump.format(b"todo: make this fucking work.")
 
 main()
